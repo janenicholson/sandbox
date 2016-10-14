fb17c2a684345d59f3096fc04703979c3cfa79ab
Jane Nicholson <jnicholson@iname.com>
2016-10-14 20:56:40 +0100

    Second tutorial
    Use the FelzenszwalbHuttenlocher image segmenter

--- webcam/src/main/java/webcam/OpenImajTutorial2.java
+++ webcam/src/main/java/webcam/OpenImajTutorial2.java
@@ -4,53 +4,34 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.openimaj.image.DisplayUtilities.createNamedWindow;
 import static org.openimaj.image.DisplayUtilities.display;
 import static org.openimaj.image.ImageUtilities.readMBF;
-import static org.openimaj.image.colour.ColourSpace.CIE_Lab;
-import static org.openimaj.image.colour.ColourSpace.RGB;
-import static org.openimaj.image.colour.ColourSpace.convert;
 import static org.openimaj.image.typography.hershey.HersheyFont.TIMES_MEDIUM;
+import static webcam.TutorialImages.BEACH_URL;
+import static webcam.TutorialImages.GRAIN_URL;
 import static webcam.TutorialImages.KINDLE_URL;
 import static webcam.TutorialImages.OPEN_IMAJ_SUPPLIED;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
 import javax.swing.JFrame;
 
 import org.openimaj.image.MBFImage;
-import org.openimaj.image.connectedcomponent.GreyscaleConnectedComponentLabeler;
 import org.openimaj.image.pixel.ConnectedComponent;
-import org.openimaj.image.processor.PrimitivePixelProcessor;
-import org.openimaj.ml.clustering.FloatCentroidsResult;
-import org.openimaj.ml.clustering.assignment.HardAssigner;
-import org.openimaj.ml.clustering.kmeans.FloatKMeans;
+import org.openimaj.image.segmentation.FelzenszwalbHuttenlocherSegmenter;
+import org.openimaj.image.segmentation.SegmentationUtilities;
 
 public class OpenImajTutorial2 {
 
 	private MBFImage input;
-	private MBFImage convertedInput;
-	private MBFImage clusteredInput;
-	private MBFImage output;
 
 	public void run() {
 		try {
-			input = readMBF(OPEN_IMAJ_SUPPLIED.toURL());
+			input = readMBF(KINDLE_URL.toURL());
 			JFrame window = createNamedWindow("Open IMAJ tutorial 2");
 			display(input, window);
-			convertedInput = convert(input, CIE_Lab);
-			SECONDS.sleep(1);
-			display(convertedInput, window);
-			FloatCentroidsResult centroids = cluster();
-			reportCentroids(centroids);
-			assignCentroids(centroids);
-			SECONDS.sleep(1);
-			display(clusteredInput, window);
 			connectComponents();
 			SECONDS.sleep(1);
-			display(clusteredInput, window);
-			output = convert(clusteredInput, RGB);
-			SECONDS.sleep(3);
-			display(output, window);
+			display(input, window);
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (InterruptedException e) {
@@ -58,45 +39,15 @@ public class OpenImajTutorial2 {
 
 	}
 
-	private FloatCentroidsResult cluster() {
-		FloatKMeans cluster = FloatKMeans.createExact(2);
-		return cluster.cluster(convertedInput.getPixelVectorNative(new float[convertedInput.getWidth() * convertedInput.getHeight()][3]));
-	}
-
-	private void reportCentroids(FloatCentroidsResult centroids) {
-		for (float[] fs : centroids.centroids) {
-			System.out.println(Arrays.toString(fs));
-		}
-	}
-
-	private void assignCentroids(FloatCentroidsResult centroids) {
-		clusteredInput = convertedInput.clone();
-		clusteredInput.processInplace(new Centroider(centroids));
-	}
-
 	private void connectComponents() {
-		GreyscaleConnectedComponentLabeler labeler = new GreyscaleConnectedComponentLabeler();
-		List<ConnectedComponent> components = labeler.findComponents(clusteredInput.flatten());
+		FelzenszwalbHuttenlocherSegmenter<MBFImage> segmenter = new FelzenszwalbHuttenlocherSegmenter<MBFImage>();
+		List<ConnectedComponent> components = segmenter.segment(input);
 		int i = 0;
 		for (ConnectedComponent comp : components) {
 			if (comp.calculateArea() < 50)
 				continue;
-			clusteredInput.drawText("Point:" + (i++), comp.calculateCentroidPixel(), TIMES_MEDIUM, 20);
+			input.drawText("Point:" + (i++), comp.calculateCentroidPixel(), TIMES_MEDIUM, 20);
 		}
-	}
-
-	private static class Centroider extends PrimitivePixelProcessor {
-		FloatCentroidsResult centroids;
-		private HardAssigner<float[], ?, ?> assigner;
-
-		public Centroider(FloatCentroidsResult centroids) {
-			this.centroids = centroids;
-			this.assigner = centroids.defaultHardAssigner();
-		}
-
-		@Override
-		public float[] processPixel(float[] pixel) {
-			return centroids.centroids[assigner.assign(pixel)];
-		}
+		SegmentationUtilities.renderSegments(input, components);
 	}
 }
--- webcam/src/main/java/webcam/TutorialImages.java
+++ webcam/src/main/java/webcam/TutorialImages.java
@@ -5,4 +5,6 @@ import java.net.URI;
 public class TutorialImages {
 	public static final URI OPEN_IMAJ_SUPPLIED = URI.create("http://static.openimaj.org/media/tutorial/sinaface.jpg");
 	public static final URI KINDLE_URL = URI.create("https://lh3.googleusercontent.com/PNb_ojblUx24gwyLrQXphlWWYc0zsL0mSl9NMJjt3vtDxWkWNzmQo_lIghlQ8Tr3iC30PkjqjSXRktYd-SnKQtkZOJaFKYCx7rD3fIziSCZU0jSSTIuO2ZvFFP9eT55xPbdlboWkYr60dmhunl8KXyYjYpstIGLVnlVa0O39RjUDsmTeshg36k5NM2dauccLDGEwTdOvFznJzAEeN7_CYyEPlRQug2ZhaWusGBnLxWYsHtJVeKRyoa8j7eBzY_Qc8_7BY5O2h6QkS2fSNERGiC3Ns-ogKmHA_FMi2vZijY7BHUN68KKHFUZ_cIE5wUcwq8h27CRw0sli0N2QdbPdKiv7qIt4mkQcObcCyQ_LS6pyKih0IDKd7jMolYZJNxPHcMfBPO-QGcK0iiJpkjlrk3kPoRo8Q9sU4EF1ULvIOPYN9BMeQGtcwUuvrINNosZhapnLEx3g4Nq0p0ZIfD1ijVMIVfTKB-ZfkorkOlK3Fmz1TaNmdVutT2ySSaDC2sDcDNPKkbXN3rWdhl_Ct8wHkr5pPBXx1JS7aZOW1YCKJpih-TtbAWGGfFdMYntUGzqTPBQenhSUfoK_OXcGiUIQW2uVSxEQYB7l0pnszyiBJNmLMB7f=w1108-h1476-no");
+	public static final URI BEACH_URL = URI.create("http://cs.brown.edu/~pff/segment/beach.gif");
+	public static final URI GRAIN_URL = URI.create("http://cs.brown.edu/~pff/segment/grain.gif");
 }
