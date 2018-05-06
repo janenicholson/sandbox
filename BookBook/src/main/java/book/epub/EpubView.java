2d1f0408c5e355281c6d9636c54b1034eb831cb2
Jane Nicholson <jnicholson@iname.com>
2018-05-06 14:26:41 +0100

    see if I can remember what this book thingy was all about

--- BookBook/src/main/java/book/epub/EpubView.java
+++ BookBook/src/main/java/book/epub/EpubView.java
@@ -7,13 +7,12 @@ import book.epub.format.EpubContentOpfFile;
 import book.epub.format.EpubMimeTypeFile;
 import book.epub.format.EpubNcxFile;
 import lombok.Data;
-import lombok.Getter;
 
 @Data
 public class EpubView {
-	@Getter private final EpubMimeTypeFile mimeType;
-	@Getter private final EpubContainerFile containerFile;
-	@Getter private final EpubContentOpfFile contentOpfFile;
-	@Getter private final EpubNcxFile ncxFile;
-	@Getter private final List<PersistableContentFile> epubData;
+	private final EpubMimeTypeFile mimeType;
+	private final EpubContainerFile containerFile;
+	private final EpubContentOpfFile contentOpfFile;
+	private final EpubNcxFile ncxFile;
+	private final List<PersistableContentFile> epubData;
 }
